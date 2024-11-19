24344ed8123da0cdc226571fa593062c8331afd7
arshad012 <forarshad99@gmail.com>
2024-11-19 14:29:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 // Complete the js code
-function Car(make, model) {}
+class Car {
+	constructor(make, model) {
+		this.make = make;
+		this.model = model;
+	}
+	getMakeModel () {
+		return `${this.make} ${this.model}`;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+// function Car(make, model) {}
+class SportsCar extends Car {
+	constructor(make, model, topSpeed) {
+		super(make, model);
+		this.topSpeed = topSpeed;
+	}
+	getTopSpeed() {
+		return this.topSpeed;
+	}
+}
+
+// function SportsCar(make, model, topSpeed) {}
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel());
+console.log(car.getTopSpeed());
+
+
 
 // Do not change the code below
 window.Car = Car;
